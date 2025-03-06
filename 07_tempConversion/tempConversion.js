86f2df6adc4c856d6b670e6591fab2a85a5c3870
Doron Pela <devronpela@gmail.com>
2025-03-06 10:02:34 +0000

    Implemented tempConversion function with rounding workaround  with on 6 specs in suite

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitValue) {
+  celciusValue = Math.round((farenheitValue - 32) * (5/9)*10)/10; 
+  return celciusValue;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusValue) {
+  farenheitValue = Math.round(((celciusValue * (9/5)) + 32)*10)/10;
+  return farenheitValue;  
 };
 
 // Do not edit below this line
