1c76463b8a9ea3a398ebc3f021a9e030e38ccf63
Doron Pela <devronpela@gmail.com>
2025-03-06 05:52:59 +0000

    Implemented and tested a reverseString method for reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string = string.split("");
+    let i = 0;
+    let j = string.length-1;
+    //to implement to string.reverse method
+    while (i<j){
+        let temp = string[i];
+        string[i] = string[j];
+        string[j] = temp;
+        i++;
+        j--;
+    }
+    string = string.join("");
+    return string;
 };
 
 // Do not edit below this line
