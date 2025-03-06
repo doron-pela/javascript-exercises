9defb116194da1ca74e85db1e4d5b3b3f611489d
Doron Pela <devronpela@gmail.com>
2025-03-06 08:05:26 +0000

    Implemented array filtering function using the f.argument iterable object

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        array = array.filter((element)=>{return element !== arguments[i]});
+    }
+    return array;
 };
 
 // Do not edit below this line
