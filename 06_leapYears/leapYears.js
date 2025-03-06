f710d55f35efb4f7d8ca668bc7251ef0bd6539e3
Doron Pela <devronpela@gmail.com>
2025-03-06 09:02:30 +0000

    Tested leapYear function for 6 specs in test suite

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    x = false;
+    if(year %4 ===0){
+        x = true;
+        if(year %100 === 0 && !(year %400===0)){
+            x = false;
+        }
+    }
+    return x;
 };
 
 // Do not edit below this line
